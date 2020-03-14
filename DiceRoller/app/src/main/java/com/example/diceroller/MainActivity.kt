569294baf7c8606c16855d2a70b1e35898d438ac
talhajavaidmalik <talha.javaid@targetofs.com>
2020-03-14 19:38:17 +0500

    Dice Roller Application

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Lets Roll"
+        rollButton.setOnClickListener {
+            //            Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_images)
+    }
+
+    private fun rollDice() {
+
+        val randomint = Random.nextInt(6) + 1
+        val drawableResource = when (randomint) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+
+
+}
---
+++ KotlinBootcamp/app/AMS.kts
@@ -0,0 +1,6 @@
+import kotlin.script.templates.standard.ScriptTemplateWithArgs
+
+fun main(args: <Array<String>){
+    print("HEllpw,word")
+
+}
